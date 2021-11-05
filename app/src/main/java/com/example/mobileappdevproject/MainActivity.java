1e247c2b39ac20bd25066f9f16614608f1dd1c90
Gizzel-Ruiz <89419407+Gizzel-Ruiz@users.noreply.github.com>
2021-11-04 19:25:16 -0500

    Main page added; also added placeholder activity and class to test onClick function

--- app/src/main/java/com/example/mobileappdevproject/MainActivity.java
+++ app/src/main/java/com/example/mobileappdevproject/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.mobileappdevproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //hide action bar from layout
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+
+        //creates layout for onClick method to allow user to click anywhere on the screen to proceed to the next activity
+        // (created activity_launch and LaunchScreen class as placeholders to test onClick functionality but these can be renamed if needed)
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.activity_main_layout);
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLaunch = new Intent(MainActivity.this, LaunchScreen.class);
+                startActivity(intentLaunch);
+            }
+        });
     }
 }
