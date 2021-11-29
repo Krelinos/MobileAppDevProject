083b78145e067ebe5a9257def26faa5e4e959511
Krelinos <emmer.palaming@gmail.com>
2021-11-28 22:38:38 -0600

    Basic, basic prototype of the final product is working. YOU NEED THE DB FILE!
    DB file is in Discord.

--- app/src/main/java/com/example/mobileappdevproject/NarrativeActivity.java
+++ app/src/main/java/com/example/mobileappdevproject/NarrativeActivity.java
@@ -1,23 +1,19 @@
 package com.example.mobileappdevproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.animation.Keyframe;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Timer;
@@ -27,10 +23,11 @@ public class NarrativeActivity extends AppCompatActivity {
 
     private DatabaseManager choicesDB;
 
-    private ImageView narrativeBackground;  // Background image that is not affect by the ScrollView
-    private LinearLayout narrativeLayout;           // LinearLayout inside ScrollView containing the narrative elements
-    private View choiceDialog;              // Template for the TextView that displays the story text
-    private View choiceButton;              // Template for the branching path buttons
+    private RelativeLayout activityNarrative;   // Main layout that holds everything except for non-game things like the menu and... yeah just the menu
+    private ImageView narrativeBackground;      // Background image that is not affect by the ScrollView
+    private LinearLayout narrativeLayout;       // LinearLayout inside ScrollView containing the narrative elements
+    private View choiceDialog;                  // Template for the TextView that displays the story text
+    private View choiceButton;                  // Template for the branching path buttons
 
     private LayoutInflater inflater;
 
@@ -50,6 +47,8 @@ public class NarrativeActivity extends AppCompatActivity {
 
         inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
 
+        //----
+
         View newNarrativeTextView = getLayoutInflater().inflate(R.layout.choice_dialog, null);
 
         TextView tv = newNarrativeTextView.findViewById(R.id.choice_dialog_tv);
@@ -62,11 +61,114 @@ public class NarrativeActivity extends AppCompatActivity {
 
         TextView what = findViewById(R.id.wa).findViewById(R.id.choice_dialog_tv);
         what.setText( oh );
+//        transitionToNewBackground( choicesDB.selectById(2) );
+    }
+
+    void populateLayoutWithChoice( Choice dialogToPopulateWith ) {
+
+        // Dialog
+        String[] sectionedDialog = dialogToPopulateWith.getDialog().split("\n");
+        TextView[] dialogTVs = new TextView[sectionedDialog.length];
+
+        for( int i = 0; i < sectionedDialog.length; i++ ) {
+
+            View newNarrativeChoiceDialog = getLayoutInflater().inflate(R.layout.choice_dialog, null);
+            newNarrativeChoiceDialog.setId( dialogToPopulateWith.getId() );
+//            newNarrativeChoiceDialog.setAlpha( 0f );
+
+            TextView tv = newNarrativeChoiceDialog.findViewById(R.id.choice_dialog_tv);
+            tv.setId( View.generateViewId() );
+            tv.setText( sectionedDialog[i] );
+
+            narrativeLayout.addView( newNarrativeChoiceDialog );
+
+        }
+
+        // Aesthetic line, then branches
+        View newAestheticLine = getLayoutInflater().inflate(R.layout.aesthetic_line, null);
+        narrativeLayout.addView( newAestheticLine );
+
+        ArrayList<Choice> choiceBranches = choicesDB.selectAllByParentId( dialogToPopulateWith.getId() );
+        for( int i = 0; i < choiceBranches.size(); i++ ) {
+
+            View newChoiceButton = getLayoutInflater().inflate( R.layout.choice_button, null );
+
+            newChoiceButton.setId( View.generateViewId() );
+            newChoiceButton.setTag( 1, choiceBranches.get(i) );
+            // Note: choice_button views automatically connect with onClickChoiceButton
+
+        }
+
+        // Now slowly reveal each TextView according to their estimated reading time.
+        // irisreading.com says the average reader reads 200 to 250 word per minute.
+
+//        fadeInTextView(  );
+
+    }
+
+    public void transitionToNewBackground( Choice initialChoice ) {
+
+        // This will cause narrativeLayout and narrativeBackground to become invisible,
+        // revealing the black background for that "fade to black" effect.
+        // When it fades back in, narrativeLayout should be clear of all dialogs and branches.
+        // The narrativeBackground should be fully opaque, then it fades slightly to give
+        // way to the new dialog coming in.
+
+        // narrativeLayout
+        Keyframe k00 = Keyframe.ofFloat( 0f, 1f );
+        Keyframe k01 = Keyframe.ofFloat( 0.167f, 0f );
+        Keyframe k02 = Keyframe.ofFloat( 0.833f, 0f );
+        Keyframe k03 = Keyframe.ofFloat( 1f, 1f );
+
+        PropertyValuesHolder pvhAlpha_activity = PropertyValuesHolder.ofKeyframe("alpha", k00, k01, k02, k03);
+        ObjectAnimator alphaAnim_activity = ObjectAnimator.ofPropertyValuesHolder( narrativeLayout, pvhAlpha_activity );
+
+        // narrativeBackground (the ImageView)
+        Keyframe k10 = Keyframe.ofFloat( 0f, 0.5f );
+        Keyframe k11 = Keyframe.ofFloat( 0.167f, 0f );
+        Keyframe k12 = Keyframe.ofFloat( 0.33f, 0f );
+        Keyframe k13 = Keyframe.ofFloat( 0.5f, 1f );
+        Keyframe k14 = Keyframe.ofFloat( 0.833f, 1f );
+        Keyframe k15 = Keyframe.ofFloat( 1f, 0.65f );
+
+        PropertyValuesHolder pvhAlpha_narrativeBG = PropertyValuesHolder.ofKeyframe("alpha", k10, k11, k12, k13, k14, k15);
+        ObjectAnimator alphaAnim_narrativeBG = ObjectAnimator.ofPropertyValuesHolder( narrativeBackground, pvhAlpha_narrativeBG );
+
+        alphaAnim_activity.setDuration(10000).start();
+        alphaAnim_narrativeBG.setDuration(10000).start();
+
+        // A timer is used here to clear the dialog once the fade out is fully active
+        Timer clearDialogTimer = new Timer();
+        clearDialogTimer.schedule(
+            new TimerTask() {
+                @Override
+                public void run() {
+                    runOnUiThread(new Runnable() {  // This thread stuff is a bit ridiculous
+                        @Override
+                        public void run() {
+                            narrativeLayout.removeAllViews();
+                            populateLayoutWithChoice( initialChoice );
+                        }
+                    });
+                }
+            }, 3000
+        );
+
+
+
+//        for( int i = 0; i < narrativeLayout.getChildCount(); i++ ) {
+//            System.out.println(getResources().getResourceEntryName(narrativeLayout.getChildAt(i).getId()));
+//            narrativeLayout.removeAllViews();
+//        }
+
     }
 
     public void onClickChoiceButton(View v) {
 
+        int parentId = ( (View)v.getParent() ).getId();     // View containing the TextView that called this method
 
+        v.setEnabled(false);
+        transitionToNewBackground( choicesDB.selectById(2) );
 
     }
 
