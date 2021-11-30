0c12c7b0249b0cb534ee79152141d8b51c34b0bf
Gizzel-Ruiz <gruiz015@jaguar.tamu.edu>
2021-11-29 20:55:02 -0600

    pushing changes
    GR pushing changes

---
+++ app/src/main/java/com/example/mobileappdevproject/PlayerStats.java
@@ -0,0 +1,25 @@
+package com.example.mobileappdevproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.LauncherActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class PlayerStats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_playerstats);
+    }
+
+    public void goBack(View v) {
+        Intent intentLaunch = new Intent(PlayerStats.this, NarrativeActivity.class);
+        startActivity(intentLaunch);
+    }
+}
